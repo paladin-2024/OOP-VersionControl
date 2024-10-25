11c8cc98fede6e26b5615c081187663e41b2b33e
paladin-2024 <161815974+paladin-2024@users.noreply.github.com>
2024-10-25 18:32:38 +0300

    Commit 3
    In the NV, in the main class, on line 10 the type of the Dell computer was Inspiron but after changes it s AlienWare

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         computers.add(new HPComputer("HP", "Silver", 512, 16, "Windows 07", 8, "Intel", "HP Pavilion", 7, 5, 10));
         computers.add(new HPComputer("HP", "Black", 512, 16, "Windows 10", 11, "Intel", "EliteBook", 7, 5, 10));
         computers.add(new Dell_PC("Dell", "Black", 256, 8, "Ubuntu", 9, "AMD", "Dell Inspiron", 5, 7));
-        computers.add(new Dell_PC("Dell", "White", 256, 8, "Windows 11", 11, "Intel", "Inspiron", 5, 3));
+        computers.add(new Dell_PC("Dell", "White", 256, 8, "Windows 11", 11, "Intel", "Dell AlienWare", 5, 3));
         computers.add(new LenovoPC("Lenovo", "White", 1024, 32, "Windows", 11, "Intel", "Lenovo ThinkPad", 9, 0));
         computers.add(new LenovoPC("Lenovo", "Grey", 1024, 32, "Linux", 6, "Intel", "ThinkPad", 7, 10));
         Polymorphism poly = new Polymorphism();
